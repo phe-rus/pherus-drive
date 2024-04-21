2633d5205b9002243792177ac4098dc4ddee85da
la-niina <la.niina.me@gmail.com>
2024-04-21 18:22:12 +0300

    init

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,7 @@ import { Header } from "@/components/header/header"
 import { DragDropZone } from "@/components/upload/drag-dropzone"
 import { DirectoryInfo, FileProp } from "@/types/property"
 import { Downloader } from "@/components/midlevel/midlevel"
-
+import { Settings } from "@/components/settings/settings"
 
 export default function Index() {
   const { toast } = useToast()
@@ -61,7 +61,6 @@ export default function Index() {
   const [isDetectedDragAndDrop, setIsDetectedDragAndDrop] = useState(false);
   const [fileData, setFileData] = useState<FileProp[]>([]);
   const [createdFolderName, createfolder] = useState("")
-  const [contextMneus, setContextMenus] = useState("")
 
   const [directoryInfo, setDirectoryInfo] = useState<DirectoryInfo | null>(null);
 
@@ -340,33 +339,6 @@ export default function Index() {
     await signOut(auth)
     refreshWebsite()
   }
-
-  const longPressHandler = (value: string) => {
-    let timeout: string | number | NodeJS.Timeout | undefined;
-    const duration = 500; // Adjust the duration as needed for the long press
-
-    const start = () => {
-      timeout = setTimeout(() => {
-        setContextMenus(value);
-      }, duration);
-    };
-
-    const cancel = () => {
-      clearTimeout(timeout);
-    };
-
-    const clickHandler = () => {
-      setContextMenus(value);
-    };
-
-    return {
-      onTouchStart: start,
-      onTouchEnd: cancel,
-      onMouseDown: start,
-      onMouseUp: cancel,
-      onClick: clickHandler,
-    };
-  };
 
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
@@ -732,73 +704,7 @@ export default function Index() {
                   </div>
                 </main>
               ) : selectedRoot.includes("Settings") ? (
-                <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10">
-                  <div className="mx-auto grid w-full max-w-6xl gap-2">
-                    <h1 className="text-3xl font-semibold">Settings</h1>
-                  </div>
-
-                  <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
-                    <nav
-                      className="grid gap-4 text-sm text-muted-foreground" x-chunk="dashboard-04-chunk-0"
-                    >
-                      <Link href="#" className="font-semibold text-primary">
-                        General
-                      </Link>
-                      <Link href="#">Security</Link>
-                      <Link href="#">Integrations</Link>
-                      <Link href="#">Support</Link>
-                      <Link href="#">Organizations</Link>
-                      <Link href="#">Advanced</Link>
-                    </nav>
-                    <div className="grid gap-6">
-                      <Card x-chunk="dashboard-04-chunk-1">
-                        <CardHeader>
-                          <CardTitle>Store Name</CardTitle>
-                          <CardDescription>
-                            Used to identify your store in the marketplace.
-                          </CardDescription>
-                        </CardHeader>
-                        <CardContent>
-                          <form>
-                            <Input placeholder="Store Name" />
-                          </form>
-                        </CardContent>
-                        <CardFooter className="border-t px-6 py-4">
-                          <Button>Save</Button>
-                        </CardFooter>
-                      </Card>
-                      <Card x-chunk="dashboard-04-chunk-2">
-                        <CardHeader>
-                          <CardTitle>Plugins Directory</CardTitle>
-                          <CardDescription>
-                            The directory within your project, in which your plugins are
-                            located.
-                          </CardDescription>
-                        </CardHeader>
-                        <CardContent>
-                          <form className="flex flex-col gap-4">
-                            <Input
-                              placeholder="Project Name"
-                              defaultValue="/content/plugins"
-                            />
-                            <div className="flex items-center space-x-2">
-                              <Checkbox id="include" defaultChecked />
-                              <label
-                                htmlFor="include"
-                                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                              >
-                                Allow administrators to change the directory.
-                              </label>
-                            </div>
-                          </form>
-                        </CardContent>
-                        <CardFooter className="border-t px-6 py-4">
-                          <Button>Save</Button>
-                        </CardFooter>
-                      </Card>
-                    </div>
-                  </div>
-                </main>
+                <Settings />
               ) : ("")}
             </>
           )}
